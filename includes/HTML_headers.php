6fdfce3054e973249b77997ee010fb1abcd945e7
javierlopezdesign <45286457+javierlopezdesign@users.noreply.github.com>
2021-08-12 03:31:28 +0100

    v0.2.3 Case St. and Tools done.

--- includes/HTML_headers.php
+++ includes/HTML_headers.php
@@ -8,3 +8,4 @@
 
 
 <!-- JS Libraries -->
+<script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
--- index.php
+++ index.php
@@ -18,9 +18,9 @@
     <nav>
         <div class="navigation">
             <img class="logo__nav" src="images/logo.svg" alt="My Logo">
+            <div id="themeToggle">🌞</div>
             <div class="menu__nav">menu</div>
         </div>
-        <div id="themeToggle">🌞</div>
 
         <ul class="menu">
             <li><a href="index.php">Home</a></li>
@@ -38,7 +38,7 @@
                 <h1>Javier Lopez<BR>is not a designer.</h1>
                 <p>Not just a developer either. Let’s call it a problem solver.<BR>Helping startups define their brand identity and stand out online.</p>
                 <a class="primaryButton" href="">Get in touch!</a>
-                <a class="secondaryButton" href="">See my CV</a>
+                <!-- <a class="secondaryButton" href="">See my CV</a> -->
                 <!-- <img src="" alt="myself"> -->
             </div>
             <img class="myPic" src="images/mypic.png" alt="mypic">
@@ -181,18 +181,233 @@
         </div>
     </div>
 </div>
-<div class="process">
-    <div class="container__process">
-        <div class="box__process">
-            <h2 class="title__process">My process</h2>
-   
-   
-   
-   
+
+<!-- CASE STUDIES -->
+
+<div class="cases">
+    <div class="container__cases">
+        <div class="box__cases">
+            <h2 class="title__cases">Case Studies</h2>
+                <div class="boxcards__cases">
+                    
+                    <div class="card__cases">
+                        <!-- <div class="backgroundCard__cases"></div> -->
+
+                        <div class="labelCard__cases">
+                            <h3>The Fifth Taste</h3>
+                            <ul class="tags__cases">
+                                <li>Branding</li>
+                                <li>Custom CMS</li>
+                                <li>Ecommerce</li>
+                                <li>Google Maps API</li>
+                            </ul>
+                            <p class="textCard__cases">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum numquam perspiciatis nulla perferendis neque veritatis autem, qui ullam fuga recusandae?</p>
+                            <a href="">Link to open -></a>
+                        </div>
+
+                    </div>
+                    <div class="card__cases">
+                        <!-- <div class="backgroundCard__cases"></div> -->
+
+                        <div class="labelCard__cases">
+                            <h3>FoxTrk</h3>
+                            <ul class="tags__cases">
+                                <li>Branding</li>
+                                <li>Custom CMS</li>
+                                <li>Ecommerce</li>
+                                <li>Google Maps API</li>
+                            </ul>
+                            <p class="textCard__cases">Lorem ipsum dolor sit amet consectetur</p>
+                            <a href="">Link to open -></a>
+                        </div>
+
+                    </div>
+                    <div class="card__cases">
+                        <!-- <div class="backgroundCard__cases"></div> -->
+
+                        <div class="labelCard__cases">
+                            <h3>Komorebi</h3>
+                            <ul class="tags__cases">
+                                <li>Branding</li>
+                                <li>Custom CMS</li>
+                                <li>Ecommerce</li>
+                                <li>Google Maps API</li>
+                            </ul>
+                            <p class="textCard__cases">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum numquam perspiciatis nulla perferendis neque veritatis autem, qui ullam fuga recusandae?</p>
+                            <a href="">Link to open -></a>
+                        </div>
+
+                    </div>
+                    <div class="card__cases">
+                        <!-- <div class="backgroundCard__cases"></div> -->
+
+                        <div class="labelCard__cases">
+                            <h3>Waters of Leith</h3>
+                            <ul class="tags__cases">
+                                <li>Branding</li>
+                                <li>Custom CMS</li>
+                                <li>Ecommerce</li>
+                                <li>Google Maps API</li>
+                            </ul>
+                            <p class="textCard__cases">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum numquam perspiciatis nulla perferendis neque veritatis autem, qui ullam fuga recusandae?</p>
+                            <a href="">Link to open -></a>
+                        </div>
+
+                    </div>
+
+                </div>
         </div>
     </div>
-
 </div>
+
+<!-- My tools -->
+<div class="tools">
+    <div class="container__tools">
+        <div class="box__tools">
+            <h2 class="title__tools">My Tools</h2>
+                <div class="boxcards__tools">
+                    
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-html"></span></div>
+                        <p class="name__tools">HTML5</p>
+                    </div>
+                    
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-css"></span></div>
+                        <p class="name__tools">CSS3</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-sass"></span></div>
+                        <p class="name__tools">Sass</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-php3"></span></div>
+                        <p class="name__tools">Php</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-python"></span></div>
+                        <p class="name__tools">Python</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-js-official"></span></div>
+                        <p class="name__tools">Javascript</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="akar-icons:github-fill"></span></div>
+                        <p class="name__tools">Github</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-vscode2"></span></div>
+                        <p class="name__tools">VSCode</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-mysql"></span></div>
+                        <p class="name__tools">MySql</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-docker2"></span></div>
+                        <p class="name__tools">Docker</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="grommet-icons:figma"></span></div>
+                        <p class="name__tools">Figma</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="logos:trello"></span></div>
+                        <p class="name__tools">Trello</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="simple-icons:notion"></span></div>
+                        <p class="name__tools">Notion</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-photoshop"></span></div>
+                        <p class="name__tools">Photoshop</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"><span class="iconify" data-inline="false" data-icon="vscode-icons:file-type-ai"></span></div>
+                        <p class="name__tools">Ilustrator</p>
+                    </div>
+                    <!-- <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div>
+                    <div class="card__tools">
+                        <div class="img__tools"></div>
+                        <p class="name__tools">tool name</p>
+                    </div> -->
+
+
+                </div>
+        </div>
+    </div>
+</div>
+
+<!-- CONTACT -->
+
+<div class="contact">
+    <div class="container__contact">
+        <div class="box__contact">
+            <h2 class="title__contact">Let's collaborate!</h2>
+                <div class="info__contact">
+                    <h4>Javier Lopez</h4>
+                    <p>Denny, Falkirk</p>
+                    <p>07963772339</p>
+                    <p>hello@javierlopez.design</p>
+                </div>
+                
+        </div>
+    </div>
+</div>
+
+<footer>
+    <p>   This is the footer</p>
+<BR><BR>
+</footer>
 
 
 
