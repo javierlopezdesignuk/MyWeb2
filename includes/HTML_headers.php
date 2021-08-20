b3f5ba7db16de66f7123d37244c77d985795749b
javierlopezdesign <45286457+javierlopezdesign@users.noreply.github.com>
2021-08-20 03:19:08 +0100

    v0.2.6.1 some tweaks to raise the performance

--- includes/HTML_headers.php
+++ includes/HTML_headers.php
@@ -1,3 +1,9 @@
+<!-- Fonts with preconnect -->
+<!-- font links here to improve performance, raised from 86 to 92 with this -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+
 <!-- CSS -->
 <link rel="stylesheet" href="css/style_mobile.css">
 
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+//   //set headers to NOT cache a page
+//   header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+//   header("Pragma: no-cache"); //HTTP 1.0
+//   header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+  //or, if you DO want a file to cache, use:
+  header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
+
+?>
+ 
+ 
  <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,11 +18,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Javierlopez.design</title>
     <!-- <link rel="stylesheet" href="css/style.css"> -->
-
     <?php
         include_once("includes/HTML_headers.php");
     ?>
-
 
 </head>
 <body>
@@ -352,7 +362,7 @@
                         <p class="name__tools">Ilustrator</p>
                     </div>
                     <div class="card__tools">
-                        <div class="img__tools"><span class="iconify" data-icon="simple-icons:webflow"></span></div>
+                        <div class="img__tools"><svg class="iconify" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M17.802 8.56s-1.946 6.103-2.105 6.607a4778.8 4778.8 0 0 0-1.484-11.473c-3.316 0-5.089 2.36-6.026 4.851l-2.565 6.637c-.015-.476-.36-6.565-.36-6.565c-.204-3.052-3-4.91-5.262-4.91l2.739 16.6c3.474-.015 5.347-2.361 6.328-4.852c0 0 2.09-5.398 2.176-5.643c.015.23 1.5 10.494 1.5 10.494c3.488 0 5.362-2.202 6.37-4.606L24 3.708c-3.445 0-5.261 2.346-6.198 4.851z" fill="currentColor"/></svg></div>
                         <p class="name__tools">Webflow</p>
                     </div>
                     <!-- <div class="card__tools">
@@ -501,19 +511,19 @@
         </div> -->
         <div class="sociallinks__footer">
             <a href="tel:+447963772339">
-                <span class="iconify icon__contactform" data-icon="jam:phone" data-inline="false"></span>
+                <svg class="iconify" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M9 2h6a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zm0 2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H9zm3 15a1 1 0 1 1 0-2a1 1 0 0 1 0 2z" fill="currentColor"/></svg>
                 <p class="detail__contact">+44 7963 772 339</p>
             </a>    
             <a href="mailto:hello@javierlopez.design" target="_blank" >
-                <span class="iconify icon__contactform" data-icon="ic:baseline-alternate-email" data-inline="false"></span>
+                <svg class="iconify" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10h5v-2h-5c-4.34 0-8-3.66-8-8s3.66-8 8-8s8 3.66 8 8v1.43c0 .79-.71 1.57-1.5 1.57s-1.5-.78-1.5-1.57V12c0-2.76-2.24-5-5-5s-5 2.24-5 5s2.24 5 5 5c1.38 0 2.64-.56 3.54-1.47c.65.89 1.77 1.47 2.96 1.47c1.97 0 3.5-1.6 3.5-3.57V12c0-5.52-4.48-10-10-10zm0 13c-1.66 0-3-1.34-3-3s1.34-3 3-3s3 1.34 3 3s-1.34 3-3 3z" fill="currentColor"/></svg>
                 <p class="detail__contact">hello@javierlopez.design</p>
             </a>    
-            <a href="https://www.instagram.com/javierlopez.design" target="_blank">
-                <span class="iconify" data-icon="carbon:logo-instagram"></span>
+            <a href="https://www.instagram.com/javierlopez.design" target="_blank" rel="noreferrer">
+                <svg class="iconify" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248a4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008a3.004 3.004 0 0 1 0 6.008z" fill="currentColor"/><circle cx="16.806" cy="7.207" r="1.078" fill="currentColor"/><path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42a4.6 4.6 0 0 0-2.633 2.632a6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71c0 2.442 0 2.753.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632a6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419a4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186c.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688a2.987 2.987 0 0 1-1.712 1.711a4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055c-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311a2.985 2.985 0 0 1-1.719-1.711a5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654c0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311a2.991 2.991 0 0 1 1.712 1.712a5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655c0 2.436 0 2.698-.043 3.654h-.011z" fill="currentColor"/></svg>
                 <p class="detail__contact">@javierlopez.design</p>
             </a>    
-            <a href="https://www.linkedin.com/in/javier-lopez-gonzalez-a094262b" target="_blank">
-                <span class="iconify" data-icon="bi:linkedin"></span>
+            <a href="https://www.linkedin.com/in/javier-lopez-gonzalez-a094262b" target="_blank" rel="noreferrer">
+                <svg class="iconify" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096a1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277c-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387c2.704 0 3.203 1.778 3.203 4.092v4.71z" fill="currentColor"/></svg>                
                 <p class="detail__contact">@javierlopez</p>
             </a>    
         </div>
